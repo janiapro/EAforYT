4c4c5e2f24cb262a267bc88b2687512a4b7313cd
janiapro <promrbrown@gmail.com>
2023-12-29 00:14:06 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,18 +5,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const apiKey = process.env.API_KEY;
+const apiKey = process.env.API_KEY; // Ensure your .env file contains API_KEY=your_actual_key
 
 type EAInput = {
     id: number | string;
     data: {
         chid: string;
-        key: string;
     };
 };
 
 type EAOutput = {
-    jobRunId: string | number;
+    jobRunId: number | string;
     statusCode: number;
     data: {
         result?: {
@@ -53,28 +52,24 @@ const app: Express = express();
 app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("External adapter says yellow");
+    res.send("External adapter says hello");
 });
 
 app.post("/", async (req: Request, res: Response) => {
     const eaInputData: EAInput = req.body;
     console.log("Request data received:", eaInputData);
 
-    // Construct the URL with the API key as a query parameter
     const url = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${eaInputData.data.chid}&key=${apiKey}`;
 
-    // Construct EA's response
     let eaResponse: EAOutput = {
-        data: {},
         jobRunId: eaInputData.id,
+        data: {},
         statusCode: 0,
     };
 
     try {
-        // Make the request with axios
         const apiResponse: AxiosResponse<YouTubeApiResponse> = await axios.get(url);
 
-        // Check if the apiResponse has the expected structure
         if (apiResponse.data && apiResponse.data.items && apiResponse.data.items.length > 0) {
             const channel = apiResponse.data.items[0];
             eaResponse.data.result = {
@@ -87,12 +82,10 @@ app.post("/", async (req: Request, res: Response) => {
 
         eaResponse.statusCode = apiResponse.status;
         console.log("Returned response", eaResponse);
-
         res.json(eaResponse);
     } catch (error: any) {
         console.log("API Response error", error);
 
-        // Improve error handling by checking if error.response exists
         if (error.response) {
             eaResponse.error = error.message;
             eaResponse.statusCode = error.response.status;
@@ -108,92 +101,3 @@ app.post("/", async (req: Request, res: Response) => {
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
-
-
-
-
-/*import process from "process";
-import express, { Express, Request, Response } from "express";
-import bodyParser from "body-parser";
-import axios, { AxiosResponse } from "axios";
-
-type EAInput = {
-    id: number | string;
-    data: {
-        chid: string;
-        key: string; // Include API key in the input data
-    };
-};
-
-type EAOutput = {
-    jobRunId: string | number;
-    statusCode: number;
-    data: {
-        result?: {
-            items?: {
-                id: string;
-                statistics: {
-                    subscriberCount: string;
-                };
-            }[];
-        };
-    };
-    error?: string;
-};
-
-const PORT = process.env.PORT || 8080;
-const app: Express = express();
-
-app.use(bodyParser.json());
-
-app.get("/", (req: Request, res: Response) => {
-    res.send("External adapter says yellow");
-});
-
-app.post("/", async (req: Request, res: Response) => {
-    const eaInputData: EAInput = req.body;
-    console.log("Request data received:", eaInputData);
-
-    // Construct the URL with the API key as a query parameter
-    const url = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${eaInputData.data.chid}&key=${eaInputData.data.key}`;
-
-    // Construct EA's response
-    let eaResponse: EAOutput = {
-        data: {},
-        jobRunId: eaInputData.id,
-        statusCode: 0,
-    };
-
-    try {
-        // Make the request with axios
-        const apiResponse: AxiosResponse = await axios.get(url);
-
-        // Check if the apiResponse has the expected structure
-        if (apiResponse.data) {
-            eaResponse.data = { result: apiResponse.data };
-        }
-
-        eaResponse.statusCode = apiResponse.status;
-        console.log("Returned response", eaResponse);
-
-        res.json(eaResponse);
-    } catch (error: any) {
-        console.log("API Response error", error);
-
-        // Improve error handling by checking if error.response exists
-        if (error.response) {
-            eaResponse.error = error.message;
-            eaResponse.statusCode = error.response.status;
-        } else {
-            eaResponse.error = "Internal Server Error";
-            eaResponse.statusCode = 500;
-        }
-
-        res.json(eaResponse);
-    }
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
-*/
